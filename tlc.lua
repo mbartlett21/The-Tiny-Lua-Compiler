3a6886809b18e7d88e2c3c3e8f20e82d7d9f4600
mbartlett21 <mjmouse9999@gmail.com>
2025-11-25 21:50:28 +1000

    Added extra to compiler

--- tlc.lua
+++ tlc.lua
@@ -15,6 +15,18 @@
   lexical scoping, upvalues (closures), and proper operator precedence.
 --]]
 
+local whitelist = {
+  ['unpack'] = true,
+  ['loadstring'] = true,
+}
+
+setmetatable(_G, {
+  __index = function(self, idx)
+    if whitelist[idx] then return nil end
+    error("unpexpected index: " .. idx)
+  end,
+})
+
 -- Converts a list into a Set (lookup table) for fast existence checking.
 --
 -- In Lua, iterating over a list to check for existence is slow. By converting
@@ -1389,7 +1401,8 @@ function Parser:parsePrimaryExpression()
       return { kind = "FunctionExpression",
         body       = body,
         parameters = parameters,
-        isVararg   = isVararg
+        isVararg   = isVararg,
+        functionName = "<anonymous fn>"
       }
     end
 
@@ -1568,7 +1581,8 @@ function Parser:parseLocalStatement()
         kind       = "FunctionExpression",
         body       = body,
         parameters = parameters,
-        isVararg   = isVararg
+        isVararg   = isVararg,
+        functionName = functionName,
       }
     }
   else
@@ -1779,6 +1793,8 @@ function Parser:parseFunctionDeclaration()
   -- Parse the base function name (e.g., `myFunc` in `myFunc.new`).
   local expression = self:consumeVariable()
 
+  local functionName = expression.name
+
   -- This loop handles `.field` and `:method` parts.
   local isMethodDeclaration = false
   while self.currentToken do
@@ -1798,6 +1814,7 @@ function Parser:parseFunctionDeclaration()
         value = fieldName
       }
     }
+    functionName = functionName .. (isDot and '.' or ':') .. expression.index.value
 
     if isColon then
       isMethodDeclaration = true
@@ -1827,7 +1844,8 @@ function Parser:parseFunctionDeclaration()
       kind       = "FunctionExpression",
       body       = body,
       parameters = parameters,
-      isVararg   = isVararg
+      isVararg   = isVararg,
+      functionName = functionName,
     } }
   }
 end
@@ -3249,6 +3267,7 @@ function CodeGenerator:processFunction(functionNode, closureRegister)
   local childProto  = self:emitPrototype({
     isVararg  = functionNode.isVararg,
     numParams = #functionNode.parameters,
+    functionName = functionNode.functionName,
   })
   self.proto = childProto
   self:processFunctionBody(functionNode)
