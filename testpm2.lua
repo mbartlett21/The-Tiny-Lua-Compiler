04b5390921f59ff8704d43246915e1aee28a719f
mbartlett21 <mjmouse9999@gmail.com>
2025-12-05 09:19:11 +1000

    Simplify anywhere
    It turns out that priority alone can get what I want

--- testpm2.lua
+++ testpm2.lua
@@ -1,6 +1,5 @@
 lpats = require'mjb_lpatterns'
 
-lpats._DEBUG=2
 
 local function teq(l, r)
 	if l ~= r then
@@ -8,8 +7,10 @@ local function teq(l, r)
 		error(string.format("l ~= r, %q ~= %q", l, r))
 	end
 end
+lpats._DEBUG=1
 teq(lpats.match('aaa', '.*a'), 'aaa')
 teq(lpats.match('aaa', '.+a'), 'aaa')
 teq(lpats.match('aba', 'ab*a'), 'aba')
-teq(lpats.match('aaab', 'a+'), 'aaa')
 teq(lpats.match('abcdefgh', 'b'), 'b')
+teq(lpats.match('aaab', 'a+'), 'aaa')
+teq(lpats.match('abbb', 'bb-'), 'b')
